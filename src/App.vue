12b275371c8959f5dbe80a8fb7b3184c365a9ed3
Lucas Batista <lfebatis@gmail.com>
2020-04-09 00:54:40 -0300

    Add Vue Router

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,34 @@
   <h1>
     {{name}}
     <MyHello />
+    <router-view>
+      
+    </router-view>
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+    <button v-on:click="goBack()">Adiciona 1</button>
   </h1>
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import MyHello from "./MyHello.vue";
+import Vue from "vue";
+import MyHello from "./MyHello.vue";
 
-  export default Vue.extend({
-    components: {
-      MyHello
-    },
-    data: function() {
-      return {
-        name: 'Hello World! 2'
-      }
-    },
-  });
+export default Vue.extend({
+  components: {
+    MyHello
+  },
+  data: function() {
+    return {
+      name: "Hello World! 2"
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.push("/foo");
+    }
+  }
+});
 </script>
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import Vue from 'vue';
 import App from './App';
+import MyHello from "./MyHello.vue";
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: MyHello },
+  { path: '/bar', component: App }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App),
-});
+}).$mount('#app')
